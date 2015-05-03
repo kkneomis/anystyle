26656875aaee73857c71b800a6b160343852d348
Simeon Kakpovi <skakpovi@gmail.com>
2015-05-03 22:27:47 +0000

    pic fixes

--- Gemfile
+++ Gemfile
@@ -22,7 +22,7 @@ gem 'cloudinary'
 gem 'acts_as_votable', '~> 0.10.0'
 gem 'bootstrap-sass', '~> 3.3.4'
 gem "acts_as_follower"
-
+gem 'geokit-rails'
 group :production do
 
 gem 'pg'
--- app/models/style.rb
+++ app/models/style.rb
@@ -1,5 +1,6 @@
 class Style < ActiveRecord::Base
   acts_as_votable
+   acts_as_mappable :auto_geocode=>{:field=>:address, :error_message=>'Could not geocode address'}
   validates :name, :description, :presence => true
   mount_uploader :image1, ImageUploader
   
